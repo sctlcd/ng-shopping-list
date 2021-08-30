385a30f2ed862489de7e74bcf115473f0da0d160
8 <lucinda.souchet@outlook.com>
2021-08-30 02:16:30 +0200

    feat: add new item

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,8 @@ var app = angular.module('ShoppingListApp', []);
 app.controller('ListController', function($scope) {
     $scope.list = ['cheese', 'meat', 'lomo', 'dry sausage']
 
+    $scope.addItem = function(z) {
+        $scope.list.push(z);
+        $scope.newItem = "";
+    }
 });
